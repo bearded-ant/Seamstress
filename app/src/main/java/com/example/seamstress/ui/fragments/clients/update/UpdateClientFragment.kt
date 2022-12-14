f57c5aebb1c8d8001a4d91e5e96da1fa1fa73564
ant <theant@mail.ru>
2022-12-14 15:36:35 +0400

    добавил обработку нажатий в фрагменте обновления клиента

--- app/src/main/java/com/example/seamstress/ui/fragments/clients/update/UpdateClientFragment.kt
+++ app/src/main/java/com/example/seamstress/ui/fragments/clients/update/UpdateClientFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.seamstress.R
-import com.example.seamstress.databinding.FragmentNewClientBinding
+import com.example.seamstress.databinding.FragmentUpdateClientBinding
 import com.example.seamstress.domain.client.Clients
 import com.example.seamstress.ui.fragments.clients.mainFragment.ClientFragment
 import com.example.seamstress.viewmodel.SeamstressViewModel
@@ -21,7 +21,7 @@ class UpdateClientFragment : Fragment() {
 
     private lateinit var seamstressViewModel: SeamstressViewModel
 
-    private var _binding: FragmentNewClientBinding? = null
+    private var _binding: FragmentUpdateClientBinding? = null
     private val binding
         get() = _binding!!
 
@@ -30,24 +30,46 @@ class UpdateClientFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentNewClientBinding.inflate(layoutInflater)
+        _binding = FragmentUpdateClientBinding.inflate(layoutInflater)
 
         seamstressViewModel = ViewModelProvider(this)[SeamstressViewModel::class.java]
+        val safeArg = UpdateClientFragmentArgs.fromBundle(requireArguments())
+        val clientId = safeArg.clientEntityId
 
-        binding.frNewClientBtnCancel.setOnClickListener {
-            findNavController().navigate(NewClientFragmentDirections.actionNewClientFragmentToClientListFragment())
-        }
-        binding.frNewClientBtnSave.setOnClickListener {
-            val client: Clients = Clients(
-                id = 0,
-                name = binding.frNewClientName.text.toString(),
-                phone = binding.frNewClientPhone.text.toString(),
-                img = R.drawable.ic_baseline_add_a_photo_24
-            )
-            seamstressViewModel.insert(client)
-            seamstressViewModel.newIdLiveData.observe(requireActivity()){
-                Toast.makeText(requireContext(), "$it", Toast.LENGTH_LONG).show()
-                findNavController().navigate(NewClientFragmentDirections.actionNewClientFragmentToClientFragment(it))
+        seamstressViewModel = ViewModelProvider(this)[SeamstressViewModel::class.java]
+        seamstressViewModel.selectClientById(clientId)
+        seamstressViewModel.selectByIdClientLiveData.observe(requireActivity()) { client ->
+            bindClientCard(client)
+
+            binding.frUpdateClientBtnSave.setOnClickListener {
+                val updatedClient: Clients = Clients(
+                    id = client.id,
+                    name = binding.frUpdateClientName.text.toString(),
+                    phone = binding.frUpdateClientPhone.text.toString(),
+                    img = R.drawable.ic_baseline_add_a_photo_24
+                )
+                seamstressViewModel.update(updatedClient)
+                Toast.makeText(requireContext(), "success update", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(
+                    UpdateClientFragmentDirections.actionUpdateClientFragmentToClientFragment(
+                        clientId
+                    )
+                )
+            }
+
+            binding.frUpdateClientBtnDelete.setOnClickListener {
+                seamstressViewModel.delete(client)
+                Toast.makeText(requireContext(), "client deleted", Toast.LENGTH_SHORT).show()
+                //todo make alert dialog
+                findNavController().navigate(UpdateClientFragmentDirections.actionUpdateClientFragmentToClientListFragment())
+            }
+
+            binding.frUpdateClientBtnCancel.setOnClickListener {
+                findNavController().navigate(
+                    UpdateClientFragmentDirections.actionUpdateClientFragmentToClientFragment(
+                        clientId
+                    )
+                )
             }
         }
 
@@ -57,5 +79,12 @@ class UpdateClientFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun bindClientCard(client: Clients) {
+        binding.frUpdateClientPhoto.setImageResource(R.drawable.ic_baseline_add_a_photo_24)
+        binding.frUpdateClientName.setText(client.name)
+        binding.frUpdateClientPhone.setText(client.phone)
+        binding.frUpdateClientBalance.setText(client.balance.toString())
     }
 }
